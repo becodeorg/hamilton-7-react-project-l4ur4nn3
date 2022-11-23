b9c4510b0b83719ebe45a0f01003f9c67c387cb8
l4ur4nn3 <nicolaslauranne99@gmail.com>
2022-11-23 15:49:41 +0100

    fetching data OK

--- src/App.js
+++ src/App.js
@@ -5,10 +5,28 @@ function App() {
 
     const apiKey = '4429ca6618a5716e1f7211f2cc56404c'
     const [weatherData, setWeatherData] = useState([{}])
+    const [city, setCity] = useState("")
+
+    const getWeather = (event) => {
+        if (event.key == "Enter") {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`).then(
+                response => response.json()
+            ).then(
+                data => {
+                    setWeatherData(data)
+                }
+            )
+        }
+    }
 
   return (
     <div className='container'>
-      <input className='input' placeholder='Enter city...'></input>
+      <input 
+      className='input' 
+      onChange={e => setCity(e.target.value)}
+      value={city}
+      onKeyPress={getWeather}
+      placeholder='Enter city... '></input>
     </div>
   )
 }
