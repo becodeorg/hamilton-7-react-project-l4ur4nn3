bf4a0d0e8bd5dd7ec7e8c164a3cc27dc6f85d28e
l4ur4nn3 <nicolaslauranne99@gmail.com>
2022-11-28 11:02:29 +0100

    starting over -- push before disaster

--- src/App.js
+++ src/App.js
@@ -1,49 +1,63 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import './App.css'
-function App() {
-    const apiKey = '4429ca6618a5716e1f7211f2cc56404c'
-    const [weatherData, setWeatherData] = useState([{}])
-    const [city, setCity] = useState("")
+import Search from './components/search/search'
 
-    const getWeather = (event) => {
-        if (event.key == "Enter") {
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`).then(
-                response => response.json()
-            ).then(
-                data => {
-                    setWeatherData(data)
-                    setCity("")
-                    console.log(data.coord.lat)
-                    console.log(data.coord.lon)
-                }
-            )
-        }
+
+
+function App() {
+
+//    const apiKey = '4429ca6618a5716e1f7211f2cc56404c'
+    const handleOnSearchChange = (searchData) => {
+        console.log(searchData)
     }
 
-    return (
-        <div className='container'>
-            <input 
-            className='input' 
-            onChange={e => setCity(e.target.value)}
-            value={city}
-            onKeyPress={getWeather}
-            placeholder='Enter city... '
-            />
+    // const [weatherData, setWeatherData] = useState([{}])
+    // const [city, setCity] = useState("")
+    // const [lon, setLon] = useState("")
+    // const [lat, setLat] = useState("")
 
-            {typeof weatherData.main === 'undefined' ? (
+    // const getWeather = (event) => {
+    //     if (event.key == "Enter") {
+    //         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`).then(
+    //             response => response.json()
+    //         ).then(
+    //             data => {
+    //                 setWeatherData(data)
+    //                 setCity("")
+    //                 // setLat(data.coord.lat)
+    //                 // setLon(data.coord.lon)
+    //                 console.log(data.coord.lon)
+    //             }
+    //         )
+    //         //     fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    //     }
+    // }
+
+    return (
+        // <div className='container'>
+        //     <input 
+        //     className='input' 
+        //     onChange={e => setCity(e.target.value)}
+        //     value={city}
+        //     onKeyPress={getWeather}
+        //     placeholder='Enter city... '
+        //     />
+
+        //     {typeof weatherData.main === 'undefined' ? (
                 <div>
-                    <p>Please enter a city</p>
+                    <Search onSearchChange={handleOnSearchChange} />
+                    {/* <p>Please enter a city</p> */}
                 </div>
-            ): (
-                <div>
-                    <div>
-                    <p>{weatherData.name}</p>
-                    <p>{Math.round(weatherData.main.temp)}°C</p>
-                    <p>{weatherData.weather[0].main}</p>
-                    </div>
-                </div>
-            )}   
-        </div>
+        //     ): (
+        //         <div>
+        //             <div>
+        //             <p>{weatherData.name}</p>
+        //             <p>{Math.round(weatherData.main.temp)}°C</p>
+        //             <p>{weatherData.weather[0].main}</p>
+        //             </div>
+        //         </div>
+        //     )}   
+        // </div> */
 
     )
 }
---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+    "X-RapidAPI-Key": "cc736f03eemsh8676a72f4f451c9p17aa36jsnf75d93c94009",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
